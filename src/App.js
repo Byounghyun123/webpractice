db5b6f1b4d49df129f97089f9ced1c94f06d442d
Byounghyun123 <kevinlee.number10@gmail.com>
2024-10-03 19:08:43 -0400

    Initiated project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center bg-gray-500 font-bold'>
+      Home Page
+    </div>
+  )
+}
+
+export default App
